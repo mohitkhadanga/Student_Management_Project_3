d2ed8b296635f24a91acf38381b85deeec16d024
mohitkhadanga <101392185+mohitkhadanga@users.noreply.github.com>
2023-06-04 18:45:17 +0530

    checking

--- src/_ATM/AtmOperationIntr.java
+++
@@ -1,8 +0,0 @@
-package _ATM;
-
-public interface AtmOperationIntr {
-    void viewBalance();
-    void withdrawAmount(double withdrawAmount);
-    void depositAmount(double depositAmount);
-    void viewMiniStatement();
-}
